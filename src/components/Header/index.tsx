c8b59334fd2b0de6e7e4fd99db05a91a26cea74f
João Dias <joaoeduardodias123@gmail.com>
2023-02-16 09:02:42 -0300

    feat: created cart button

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logoImg from '../../assets/logo.svg'
+import { Cart } from '../Cart'
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Link href="/">
+        <Image src={logoImg} alt="" />
+      </Link>
+      <Cart />
+    </HeaderContainer>
+  )
+}
